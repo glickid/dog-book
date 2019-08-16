5274df21a257b51e2e95860cc7856c0204e444a6
glickid <glickid@yahoo.com>
2019-08-16 16:35:22 +0300

    replace API

--- App/dogBook/dogBookSrv.js
+++ App/dogBook/dogBookSrv.js
@@ -1,12 +1,14 @@
 dogBook_app.factory('dogBookSrv', function ($http, $log, $q, $timeout) {
 
-    function Dog(breed, randomPic, picList) {
+    function Dog(breed, id, randomPic, picList) {
         this.breed = breed;
         //this.subBreed = subBreed;
+        this.id = id;
         this.picList = picList;
         this.randomPic = randomPic;
     }
     var dogArr = [];
+    var arrIndex = -1;
 
     function getDogsArr()
     {
@@ -14,27 +16,28 @@ dogBook_app.factory('dogBookSrv', function ($http, $log, $q, $timeout) {
     }
 
     function getBreedList() {
-        var listBreedUrl = "https://dog.ceo/api/breeds/list/all"
+        // var listBreedUrl = "https://dog.ceo/api/breeds/list/all"
+        var listBreedUrl = "https://api.thedogapi.com/v1/breeds?x-api-key=452cf44b-f472-4a37-a4fb-a1e3e2bcdebd";
         var async = $q.defer();
 
         if (dogArr.length === 0) {
             $http.get(listBreedUrl).then(function (success) {
 
-                angular.forEach(success.data.message, function (key, value) {
-                    if (key.length) {
-                        for (var i = 0; i < key.length; i++) {
+                angular.forEach(success.data, function (key) {
+                    // if (key.length) {
+                    //     for (var i = 0; i < key.length; i++) {
 
-                            //there are no images for coton-detulear on server skip it!
-                            if (value + "-" + key[i] === "coton-detulear")
-                                continue;
-                            var dog = new Dog(value + "-" + key[i], "", []);
-                            dogArr.push(dog);
-                        }
-                    }
-                    else {
-                        var dog = new Dog(value, "", []);
+                    //         //there are no images for coton-detulear on server skip it!
+                    //         if (value + "-" + key[i] === "coton-detulear")
+                    //             continue;
+                    //         var dog = new Dog(value + "-" + key[i], "", []);
+                    //         dogArr.push(dog);
+                    //     }
+                    // }
+                    // else {
+                        var dog = new Dog(key.name, key.id, "", []);
                         dogArr.push(dog);
-                    }
+                    // }
                 });
                 async.resolve(dogArr);
             }, function (error) {
@@ -46,20 +49,24 @@ dogBook_app.factory('dogBookSrv', function ($http, $log, $q, $timeout) {
         return async.promise;
     }
 
-    function getImagesRandomForBreed(breed, index) {
-        var randomBreedUrl = "https://dog.ceo/api/breed/" + breed + "/images/random"
+    function getImagesRandomForBreed(breed_id, index) {
+        var randomBreedUrl = "https://api.thedogapi.com/v1/images/search?breed_id=" + breed_id + "&size=thumb&limit=1&format=json&x-api-key=452cf44b-f472-4a37-a4fb-a1e3e2bcdebd"
+        // "https://dog.ceo/api/breed/" + breed + "/images/random"
         var async = $q.defer();
 
         $http.get(randomBreedUrl).then(function (success) {
 
-            dogArr[index].randomPic = success.data.message;
+            if (success.data.length > 0) {
+                dogArr[index].randomPic = success.data["0"]["url"]; //message;
+            } else {
+                dogArr[index].randomPic = "/assets/placeholder.jpg";
+            }
             async.resolve(dogArr);
-
         }, function (error) {
 
 
             $log.log(error);
-            dogArr[index].randomPic = "/assests/placeholder.jpg"
+            dogArr[index].randomPic = "/assets/placeholder.jpg";
             async.reject("failed to get image for " + breed);
 
         });
@@ -72,7 +79,7 @@ dogBook_app.factory('dogBookSrv', function ($http, $log, $q, $timeout) {
         var async = $q.defer();
 
         for (var i = 0; i < dogArr.length; i++) {
-            premises.push($timeout(getImagesRandomForBreed(dogArr[i].breed, i),
+            premises.push($timeout(getImagesRandomForBreed(dogArr[i].id, i),
                 (100 + (30 * i))));
         }
 
@@ -91,29 +98,33 @@ dogBook_app.factory('dogBookSrv', function ($http, $log, $q, $timeout) {
     {
         var async = $q.defer();
         var index = 0;
-        var breedUrl = "https://dog.ceo/api/breed/" + breed + "/images";
+        var breed_id = -1;
+        // "https://dog.ceo/api/breed/" + breed + "/images";
 
-        for(var index =0;index<dogArr.length; index++)
+        for(;index<dogArr.length; index++)
         {
             if(breed === dogArr[index].breed)
             {
                 //async.promise = getDogGallery(dogArr[index]);
+                breed_id = dogArr[index].id;
+                arrIndex = index;
                 break;
             }
         }
         
-        $http.get(breedUrl).then(function (success, index) {
-        
-            if (index < dogArr.length)
-            {
-                dogArr[index].picList = success.data.message;
-                async.resolve(dog.picList);
-            }
-            else
-            {
-                async.resolve(success.data.message);
-            }
+        if ((breed_id == -1) || (index > dogArr.length-1)) 
+            return null;
 
+        var breedUrl = "https://api.thedogapi.com/v1/images/search?breed_id=" + breed_id + "&size=thumb&limit=100&format=json&x-api-key=452cf44b-f472-4a37-a4fb-a1e3e2bcdebd"
+
+        $http.get(breedUrl).then(function (success) {
+        
+            for(var j=0; j<success.data.length; j++) {
+                dogArr[arrIndex].picList.push(success.data[j].url);
+            }
+            
+            arrIndex = -1;
+            async.resolve(dogArr[index].picList);
         }, function (error) {
 
             $log.log(error);
